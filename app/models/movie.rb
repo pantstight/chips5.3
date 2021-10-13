ee71de7976a4c53c744e972e4bd3063e84e79133
Matt Chen <muchichen@berkeley.edu>
2021-10-13 00:18:51 +0000

    part 1 attempt0

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+   Movie.distinct.pluck(:rating)
+  end
+  
+  def self.with_ratings(ratings_list)
+    ratings_list.nil? Movie.all : Movie.where(:rating ratings_list)
+  end
 end
